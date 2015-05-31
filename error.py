c27e43ce93392cc8fab002e2ccf351f2b3a2c035
yhf <xdhfying@gmail.com>
2015-05-31 11:36:54 +0800

    update error message

--- error.py
+++ error.py
@@ -7,4 +7,4 @@ class ItemTypeError(Exception):
         self.item_type = item_type
 
     def __str__(self):
-        return "%s type item should not exist here." % self.item_type
+        return "%s item should not exist here." % self.item_type.__name__
